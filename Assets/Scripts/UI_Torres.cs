98aac7c0e598d6d4a6ac5d8f0316a4921521a51f
saramj16 <sara.marti@students.salle.url.edu>
2020-12-28 17:51:53 +0100

    Inventari

--- Assets/Scripts/UI_Torres.cs
+++ Assets/Scripts/UI_Torres.cs
@@ -9,10 +9,15 @@ public class UI_Torres : MonoBehaviour
 
     public GameObject itemsParent;
 
+    public GameObject TorreButton;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < torres.Count; i++)
+        {
+            
+        }
     }
 
     // Update is called once per frame
