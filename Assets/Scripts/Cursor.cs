dcbf803d643b546340a62f3be538f256ec9815d9
CinS12 <arturmarti12@gmail.com>
2021-01-04 10:09:11 +0100

    Player shoot introduction

---
+++ Assets/Scripts/Cursor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cursor : MonoBehaviour
+{
+    public Texture cursorImage;
+    Vector3 mousePos;
+    public static Vector3 tmp;
+    private RaycastHit hit;
+
+
+    void Start()
+    {
+        UnityEngine.Cursor.visible = false;
+    }
+
+    void OnGUI()
+    {
+       
+        mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
+        //Rect pos = new Rect(mousePos.x, Screen.height - mousePos.y, -cursorImage.width / 2, -cursorImage.height / 2);
+        Rect pos = new Rect(Screen.width/2 - 20, (Screen.height/2 - 20), 50, 63);
+        //Rect pos = new Rect(mousePos.x - 20, (Screen.height - 20) - mousePos.y, 50, 63);
+        GUI.Label(pos, cursorImage);
+    }
+}
---
+++ Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+
+    public Transform pivotArma;
+    public Transform puntBales;
+    private Transform target;
+
+    public GameObject balaPrefab;
+    public Camera cam;
+    public Collider collider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GameObject.Find("Camera").GetComponent<Camera>();
+        //int layerMask = 1 << 30;
+        //layerMask = ~layerMask;
+    }
+
+    // Update is called once per frame
+    void Update()
+
+    {
+        //Debug.DrawRay(cam.transform.position, cam.transform.forward, Color.yellow);
+        if (Input.GetKeyUp(KeyCode.H))
+        {
+            /**
+             Debug.Log("Ha disparat");
+             target = cam.transform.forward;
+             Debug.DrawRay(transform.position, target, Color.yellow, 5f);
+             int layerMask = 1 << 30;
+             layerMask = ~layerMask;
+             RaycastHit hit;
+             //LayerMask.GetMask("HitTest")
+             if (Physics.Raycast(transform.position, target, out hit, Mathf.Infinity, layerMask))
+             {
+                 var localHit = transform.InverseTransformPoint(hit.point);
+                 Debug.DrawLine(cam.transform.position, localHit, Color.red, 5f);
+                 Debug.Log("Did Hit");
+             }
+            **/
+            //Debug.Log("Ha disparat");
+            RaycastHit hit;
+            var ray = new Ray(cam.transform.position, cam.transform.forward);
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider != null)
+                {
+                    Debug.DrawLine(transform.position, hit.point, Color.red, 5f);
+                    Dispara();
+                }
+            }
+
+        }
+    }
+
+    void Dispara()
+    {
+        GameObject balaGO = (GameObject)Instantiate(balaPrefab, puntBales.position, puntBales.rotation);
+        Bala bala = balaGO.GetComponent<Bala>();
+
+        if (bala != null)
+            bala.Target(target);
+
+    }
+}
