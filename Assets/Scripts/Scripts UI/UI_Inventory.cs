59e90fdb5db50997846aa4ecba1cd347939e06b3
saramj16 <sara.marti@students.salle.url.edu>
2021-03-01 13:57:09 +0100

    Mides modificades UI

---
+++ Assets/Scripts/Scripts UI/UI_Inventory.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Inventory : MonoBehaviour
+{
+
+
+    private Inventory inventory;
+    private Transform itemSlotContainer;
+    private Transform itemSlotTemplate;
+
+    public static UI_Inventory instance;
+
+
+    public void SetInventory(Inventory inventory) {
+        this.inventory = inventory;
+        
+    }
+
+    public Inventory GetInventory()
+    {
+        return inventory;
+    }
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        itemSlotContainer = transform.Find("ItemsParent");
+        //Debug.Log(itemSlotContainer);
+        instance = this;
+    }
+
+    public void RefreshInventoryItems() {   //Funcio per actualitzar els nombres del inventari a la UI
+        int i = 0;
+        foreach (Transform itemSlot in itemSlotContainer)
+        {
+            Text text = itemSlot.GetComponentInChildren<Text>();
+            if (text == null) {
+                Debug.Log("No es troba text");
+
+            }
+            else
+            {
+                //Debug.Log("Text " + text.text);
+                //Debug.Log("Text inventari " + inventory.items[i].amount);
+                text.text = inventory.items[i].amount.ToString();
+            }
+            i++;
+        }
+
+    }
+}
---
+++ Assets/Scripts/Scripts UI/UI_Upgrade.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Upgrade : MonoBehaviour
+{
+
+    GameObject torreUpgrade = null;
+    public Text titol;
+    // Start is called before the first frame update
+    void Start()
+    {
+   
+        titol.text = "Upgrade " + torreUpgrade.GetComponent<Turret>().nomTorre + "?";
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OmpleTorreUpgrade(GameObject torre)
+    {
+        Debug.Log("Omple la torre");
+        torreUpgrade = torre;
+    }
+
+    public void OnClickYes()
+    {
+        Debug.Log("Ha clickat yes");
+
+        // Actualitza el q sigui i close
+        Debug.Log("Actualitza torre " + torreUpgrade.name);
+
+        torreUpgrade.GetComponent<Turret>().UpgradeTorre();
+    }
+
+    public void OnClickNo()
+    {
+        Debug.Log("Ha clickat no");
+        this.gameObject.SetActive(false);
+    }
+}
