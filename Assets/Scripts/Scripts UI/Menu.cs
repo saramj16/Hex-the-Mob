aa56b9bec4fd71257e0aaaec3eb927c35ff39f66
saramj16 <sara.marti@students.salle.url.edu>
2021-03-16 14:28:14 +0100

    Main Menu in process

--- Assets/Scripts/Scripts UI/Menu.cs
+++ Assets/Scripts/Scripts UI/Menu.cs
@@ -1,24 +1,76 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+
+    public GameObject menu;
+    public GameObject options;
+    public GameObject credits;
+    public GameObject selectMap;
+
+    public Slider sliderMusic;
+    public Slider sliderEffects;
     // Start is called before the first frame update
-    void Start()
+    public void OnClickPlay()
     {
-        
+        menu.gameObject.SetActive(false);
+        selectMap.gameObject.SetActive(true);
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnClickOptions()
     {
-        
+        menu.gameObject.SetActive(false);
+        options.gameObject.SetActive(true);
     }
 
-    public void OnClickStart()
+    public void OnClickCredits()
+    {
+        menu.gameObject.SetActive(false);
+        credits.gameObject.SetActive(true);
+    }
+
+    public void GoBack()
+    {
+        menu.gameObject.SetActive(true);
+        selectMap.gameObject.SetActive(false);
+        options.gameObject.SetActive(false);
+        credits.gameObject.SetActive(false);
+    }
+
+    public void ModifcarAudioMusic()
+    {
+       // Debug.Log("Volum musica a: " + sliderMusic.value);
+       float volum = sliderMusic.value;
+    }
+
+
+    public void ModifcarAudioEffects()
+    {
+       // Debug.Log("Volum efectes a: " + sliderEffects.value);
+        float volum = sliderEffects.value;
+    }
+    public void OnClickExit()
+    {
+        Application.Quit();
+    }
+
+    public void Mapa1()
     {
         SceneManager.LoadScene("SampleScene");
+    }
+
+    public void Mapa2()
+    {
+        //SceneManager.LoadScene("SampleScene");
+    }
+
+    public void Mapa3()
+    {
+        //SceneManager.LoadScene("SampleScene");
     }
 }
